7761d8f5d88ed8dcc6ef2bb9b07078a8f90f9b66
Reem Mohamed <166386701+reem38519753@users.noreply.github.com>
2025-01-14 05:12:36 +0200

    first upload

---
+++ src/components/Projects/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From Reacts. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'React' ?
+            <ToggleButton active value="React" onClick={() => setToggle('React')}>React</ToggleButton>
+            :
+            <ToggleButton value="React" onClick={() => setToggle('React')}>React</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Web Designer' ?
+            <ToggleButton active value="Web Designer" onClick={() => setToggle('Web Designer')}>Web Design</ToggleButton>
+            :
+            <ToggleButton value="Web Designer" onClick={() => setToggle('Web Designer')}>Web Design</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Systems' ?
+            <ToggleButton active value="Systems" onClick={() => setToggle('Systems')}>Systems</ToggleButton>
+            :
+            <ToggleButton value="Systems" onClick={() => setToggle('Systems')}>Systems</ToggleButton>
+          }
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ src/data/constants.js
@@ -0,0 +1,402 @@
+import hady from "../images/hady.png";
+import mohamed from "../images/mohamed.png";
+import beauty from "../images/beauty.png";
+import blur from "../images/blur.png";
+import covid_19 from "../images/covid-19.png";
+import mystore from "../images/mystore.png";
+import masar from "../images/masar.png";
+import system from "../images/system ERP.png";
+import monsters from "../images/monsters.png";
+
+export const Bio = {
+  name: "Hady Rabie",
+  roles: [
+    "Software Engineer",
+    "Fronend Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "Frontend developer provide comprehensive service, can participate in projects involving databases and create user interface web sites.",
+  github: "https://github.com/hadyronaldo2030",
+  resume:
+    "https://drive.google.com/file/d/121j3rY0X1ZyujCyIlAQwetv7fQv9kYjX/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/hady-rabie-57a461335/",
+  twitter: "https://x.com/Hady7Ronaldo",
+  insta: "https://www.instagram.com/hady_rapia.7/",
+  whatsApp: "https://wa.me/+201030804922",
+  telegram: "https://t.me/hadyrabie",
+  facebook: "https://www.facebook.com/eng.hady.oracle/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "https://icon.icepanel.io/Technology/svg/React.svg",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "https://icon.icepanel.io/Technology/png-shadow-512/Next.js.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "jQuery",
+        image:
+          "https://icon.icepanel.io/Technology/svg/jQuery.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Tailwind-CSS.svg",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Material-UI.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://www.svgrepo.com/show/331724/github-code-source.svg",
+      },
+      {
+        name: "MY SQL",
+        image:
+          "https://cdn.icon-icons.com/icons2/1495/PNG/512/mysqlworkbench_102952.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://www.svgrepo.com/show/376339/netlify.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Visual-Studio-Code-%28VS-Code%29.svg",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Postman.svg",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Canva.svg",
+      },
+    ],
+  },
+  {
+    title: "UI/UX",
+    skills: [
+      {
+        name: "Adobe XD",
+        image:
+          "https://w7.pngwing.com/pngs/213/165/png-transparent-adobe-logo-logos-xd-logos-and-brands-icon-thumbnail.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://cdn.prod.website-files.com/64f6a7b65b5aa4b4fbca8590/64f6dc2d7e56444752b9d4a2_figma-3d-logo.svg",
+      },
+      {
+        name: "Photoshop",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/92/Adobe_Photoshop_CS6_icon.svg",
+      },
+    ],
+  },
+
+];
+
+export const experiences = [
+  {
+    id: 1,
+    img: "",
+    role: "Frontend | Business Logic | UI/UX Design",
+    company: "Hoverloud (Freelance)",
+    date: "Aug 2024 - July 2025",
+    desc: "I worked within freelance platforms with a team and we implemented a store. I fully planned the project and created a distinctive design that was responsive to all screens and included many details.",
+    skills: [
+      "React",
+      "Html",
+      "Css3",
+      "Java Script",
+      "JQuery",
+      "Bootstrap",
+      "Adobe-Xd",
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "",
+    role: "Frontend | Business Logic | UI/UX Design",
+    company: "Masar Agency Group (ERP System)",
+    date: "Mar 2023 - Fep 2024",
+    desc: "Work on establishing an employee and customer management system for a marketing organization , complete project planning, innovative front- end design that makes it easy for administrative personnel to control with ease, Creating the front end of an electronic website that highlights the company’s most important work.",
+    skills: [
+      "Html",
+      "Css3",
+      "Java Script",
+      "JQuery",
+      "Bootstrap",
+      "Adobe-Xd",
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "",
+    role: "Web Design | Landing Pages",
+    company: "Instant",
+    date: "Aug 2021 - July 2022",
+    desc: "I trained at Instant and created landing pages such as the Covid 19 website Gym monsters",
+    skills: [
+      "Html",
+      "Css3",
+      "Java Script",
+      "JQuery",
+      "Bootstrap",
+    ],
+    doc: "",
+  },
+
+];
+
+export const projects = [
+
+  {
+    id: 0,
+    title: "My Store (E-Commerce)",
+    date: "Aug 2024 - Dec 2024",
+    description: "I worked on an online store to sell products and make it easier for the merchant to display his products with an attractive user interface and activate online purchases.",
+    image: mystore,
+    tags: [
+      "php",
+      "Laravel",
+      "MY SQL",
+      "Html",
+      "Css",
+      "React JS",
+      "Axios",
+      "Bootstrap",
+    ],
+    category: "React",
+    github: "https://github.com/hadyronaldo2030/back_end_ecommerce",
+    webapp: "",
+    member: [
+      {
+        name: "Hady Rabie",
+        img: hady,
+        linkedin: "https://www.linkedin.com/in/hady-rabie-57a461335/",
+        github: "https://github.com/hadyronaldo2030",
+      },
+      {
+        name: "Mohamed Salah",
+        img: mohamed,
+        linkedin: "https://www.linkedin.com/in/mohamed-m-salah/",
+        github: "https://github.com/Eng-Mohamed-Salah/",
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: "System ERP",
+    date: "Apr 2023 - Feb 2024",
+    description: "#Email: admin@gmail.com	#Password: 123456789 I designed and planned the management system And created an attractive and easy front-end The system includes the following features... -The main interface of the system - The responsible control unit section dashbord - The human resources management section HR - The financial management section fiance - The manager control unit manager - The employee's personal page profile - The plans and packages section plans - The clients section client - The deleted and retrieved page history - The services page services",
+    image: system,
+    tags: [
+      "php",
+      "Laravel",
+      "MY SQL",
+      "Html",
+      "Css",
+      "JS",
+      "JQuery",
+      "Bootstrap",
+    ],
+    category: "Systems",
+    github: "https://youtu.be/b3zSWU1vJLQ",
+    webapp: "",
+    member: [
+      {
+        name: "Hady Rabie",
+        img: hady,
+        linkedin: "https://www.linkedin.com/in/hady-rabie-57a461335/",
+        github: "https://github.com/hadyronaldo2030",
+      },
+      {
+        name: "Mohamed Salah",
+        img: mohamed,
+        linkedin: "https://www.linkedin.com/in/mohamed-m-salah/",
+        github: "https://github.com/Eng-Mohamed-Salah/",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Blur.TV (Movies)",
+    date: "Jan 2023",
+    description: "A distinctive design for a movie and series website where movies are displayed in three dimensions and cards are displayed in an attractive way with a beautiful user interface that is responsive to all screens.",
+    image: blur,
+    tags: [
+      "Html",
+      "Css",
+      "JS",
+      "JQuery",
+      "Bootstrap",
+    ],
+    category: "Web Designer",
+    github: "https://github.com/hadyronaldo2030/Blur.TV",
+    webapp: "https://blur-tv-hoverlou.web.app/",
+  },
+  {
+    id: 2,
+    title: "Beauty",
+    date: "July 2024 - July 2024",
+    description: "I built a beauty website that sells herbal products, oils, and books relaxation sessions. This design is simple and has a control panel to track sales statistics.",
+    image: beauty,
+    tags: ["Html", "Css", "JS", "JQuery", "Bootstrap",],
+    category: "Web Designer",
+    github: "https://github.com/hadyronaldo2030/SalonServices",
+    webapp: "https://beauty-hoverloud.web.app/",
+  },
+  {
+    id: 10,
+    title: "Masar Group",
+    date: "Mar 2024",
+    description: "An introductory website for Masar Company, through which the company’s marketing tasks and marketing plans are displayed, and it supports Arabic and English languages.",
+    image: masar,
+    tags: ["Html", "Css", "JS", "JQuery", "Bootstrap",],
+    category: "Web Designer",
+    github: "https://github.com/hadyronaldo2030/Website-Marketing-masar",
+    webapp: "Website-Marketing-masar",
+  },
+  {
+    id: 3,
+    title: "Monsters GYM",
+    date: "Oct 2022",
+    description: "Gym landing page, displaying workout schedule and some activities",
+    image: monsters,
+    tags: ["Html", "Css", "JS", "JQuery", "Bootstrap",],
+    category: "Web Designer",
+    github: "https://github.com/hadyronaldo2030/Monsters-GYM-Responsive",
+    webapp: "https://hadyronaldo2030.github.io/Monsters-GYM-Responsive/",
+  },
+  {
+    id: 4,
+    title: "Covid-19",
+    date: "July 2021",
+    description: "Landing page showing statistics of patients infected with the Corona virus and the number of deaths",
+    image: covid_19,
+    tags: ["Html", "Css", "JS", "JQuery", "Bootstrap",],
+    category: "Web Designer",
+    github: "https://github.com/hadyronaldo2030/final-project-covid-19",
+    webapp: "https://hadyronaldo2030.github.io/final-project-covid-19",
+  },
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://yt3.googleusercontent.com/K8WVrQAQHTTwsHEtisMYcNai7p7XIlyEAdZg86qYw78ye57r5DRemHQ9Te4PcD_v98HB-ZvQjQ=s900-c-k-c0x00ffffff-no-rj",
+    school: "Google (Introduction to Git and GitHub)",
+    date: "Oct 2021 - Sep 2025",
+    grade: "95.3%",
+    desc: "an online non-credit course authorized by Google and offered through Coursera",
+    degree: "",
+  },
+  {
+    id: 1,
+    img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-university-assets.s3.amazonaws.com/74/7ae340ec6911e5b395490a2a565172/JHU-Logo-Square-Mini_180px.png?auto=format%2Ccompress&dpr=1&w=180&h=180",
+    school: "Johns Hopkins University (Web Development)",
+    date: "J ul 11, 2021",
+    grade: "90%",
+    desc: "an online non-credit course authorized by Johns Hopkins University and offered through Coursera",
+    degree: "",
+  },
+  {
+    id: 2,
+    img: "https://yt3.googleusercontent.com/ytc/AIdro_nJteQVkAu1W-rpXrFWo7gtxHLfVfhdNnneD_8MCVGdeMc=s900-c-k-c0x00ffffff-no-rj",
+    school: "University of Michigan (Introduction to HTML5)",
+    date: "Jul 11, 2021",
+    grade: "92.3%",
+    desc: "an online non-credit course authorized by University of Michigan and offered through Coursera",
+    degree: "",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzZLG6PX_tdcBz58B7wu8NS2NGE984kf74bQ&s",
+    school: "Instant (Full stack Developent)",
+    date: "Jul 11, 2021",
+    grade: "92.3%",
+    desc: "This certificate is presented by Instant Software Solutions in honor of the student indicating his completion of an intensive six-month training course in the field of Web Developer The certificate reflects his commitment to learning advanced skills and gaining distinctive practical experience in software development It represents a great achievement that enhances his career opportunities in the fields of information technology and programming.",
+    degree: "",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZvyfOl34WPVvdmPuvIUGOdtFBjpzJJ-zweIxBCJFvbRoVHmtghLhO38KfVLCF1-HleDA&usqp=CAU",
+    school: "EGI",
+    date: "May 2021",
+    grade: "Excellent grade",
+    desc: "This certificate is issued by EGI University of Computer Science and Information Systems. It confirms that the student has obtained a Bachelor's degree in Information Systems with an Excellent grade. The certificate highlights exceptional academic performance across all years of study. It opens up pathways to various opportunities in the fields of information technology",
+    degree: "",
+  },
+  {
+    id: 2,
+    img: "https://immrsvstudios.com/wp-content/uploads/2024/09/1024-px-X-1024-px-identity-05-scaled.jpg.webp",
+    school: "Egypt Fwd Udacity (Web Development)",
+    date: "Aug 2021",
+    grade: "92.3%",
+    desc: "Issued by the Egypt FWD initiative, supported by the Ministry of Communications and Information Technology of Egypt. Certified by global partners such as Udacity and ITIDA. Demonstrates proficiency in fundamental web development skills. Enhances career opportunities in the tech and software development fields. Part of a program aimed at empowering youth digitally and preparing them for the modern job market. Reflects a commitment to continuous learning and staying updated with the latest web development technologies.",
+    degree: "",
+  },
+];
+
+// export const TimeLineData = [
+//   { year: 2017, text: "Started my journey" },
+//   { year: 2018, text: "Worked as a freelance developer" },
+//   { year: 2019, text: "Founded JavaScript Mastery" },
+//   { year: 2020, text: "Shared my projects with the world" },
+//   { year: 2021, text: "Started my own platform" },
+// ];
